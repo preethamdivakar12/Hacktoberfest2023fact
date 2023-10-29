6c845a927643eea21d07ccf4cc29639d3e2a35e9
Preetham Divakar <140415988+preethamdivakar12@users.noreply.github.com>
2023-10-29 20:09:43 +0530

    Create Factorial.py
     we'll contribute a new feature to a fictional Python library for working with mathematical operations.

---
+++ Factorial.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    """Calculate the factorial of a non-negative integer."""
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers.")
+    if n == 0:
+        return 1
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
